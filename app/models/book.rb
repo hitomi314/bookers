55d84cccdfa7b4ac41ce341c45cb39ca269c957a
EC2 Default User <boc.jrock@gmail.com>
2021-06-10 05:38:29 +0000

    [Fix]db:migrate,[Update]home.html,route.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,25 +1,2 @@
 class Book < ApplicationRecord
-
-  def index
-    @books = Book.all
-  end
-
-  def new
-    @book = Book.new
-  end
-
-  def create
-    book = Book.new(book_params)
-    book.save
-    if book.save
-      redirect_to root_path, success: "Book was successfully created."
-    else　render :new
-    end
-  end
-
-   private
-  # ストロングパラメータ
-  def list_params
-    params.require(:book).permit(:title, :body)
-  end
 end
--- db/migrate/20210610043749_create_books.rb
+++ db/migrate/20210610043749_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :title
-      t.test :body
+      t.text :body
 
       t.timestamps
     end
