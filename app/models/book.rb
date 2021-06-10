bd07a360abfecbd38a8a2e24afb97a4d57cc0753
EC2 Default User <boc.jrock@gmail.com>
2021-06-10 04:06:45 +0000

    [Add] home.html,index.html,route=index.new.create

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,20 @@
 class Book < ApplicationRecord
   
+  def index
+    @books = Book.all
+  end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    if book.save
+      redirect_to root_path, success: "Book was successfully created."
+    else　render :new
+    end
+  end
+  
 end
